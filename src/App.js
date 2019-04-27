790ba0de4befe95d3bb7031a7a10d33f907173b6
moguera57 <josephbanwart@gmail.com>
2019-04-26 21:18:30 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import NoMatch from './components/NoMatch'
-import About from './components/About'
-import Resume from './components/Resume'
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/resume" component={Resume} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
