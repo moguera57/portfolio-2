728351712a343135f8f30b6d09dd1dfb7cc65481
moguera57 <josephbanwart@gmail.com>
2019-04-27 09:11:50 -0400

    Updates

---
+++ precache-manifest.96894663828dc45c5fb10a42b8253810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144ae85ff1e7a407dec572baa9f114b2",
+    "url": "/portfolio-2/index.html"
+  },
+  {
+    "revision": "5cd4a6cc02e21bae17cc",
+    "url": "/portfolio-2/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio-2/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5cd4a6cc02e21bae17cc",
+    "url": "/portfolio-2/static/js/main.2238ccb5.chunk.js"
+  },
+  {
+    "revision": "d97e7774dc2181322827",
+    "url": "/portfolio-2/static/js/runtime~main.afccf5ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
